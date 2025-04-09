9bd974e284d91ab1b2991549068af197dd37a423
Nicolas-NL <nicolas-nl@hotmail.com>
2025-04-09 11:34:05 -0300

    botao para voltar a pagina inicial

--- src/pages/AnswersPage.jsx
+++ src/pages/AnswersPage.jsx
@@ -15,6 +15,10 @@ function AnswersPage() {
     <div className="flex flex-col items-center justify-center bg-slate-100 rounded-lg p-6 gap-4 shadow-lg border border-black w-full max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">Resultado do Quiz</h1>
       <p className="text-xl mb-6">Você acertou {correctAnswersCount} de {questions.length} perguntas!</p>
+      <button onClick={startPageClick} className="h-10 bg-slate-300 p-2 rounded-md border border-black mb-4">
+        Voltar para o início
+
+      </button>
       </div>
   );
 }
