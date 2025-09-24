63288142a3e34ebc43174261e920d2533e16eda4
Nicolas-NL <nicolas-nl@hotmail.com>
2025-09-24 20:42:10 -0300

    pequeno ajuste tamanho dos botoes

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -2,7 +2,7 @@ function MainButton({className= "", ...props}) {
   return (
     <button
       {...props}
-      className="bg-blue-900 w-80 h-auto font-sans font-bold p-2 rounded-md  text-white border border-black gap-2 items-center justify-center flex disabled:opacity-50 " 
+      className="bg-blue-900 w-60 h-auto font-sans font-bold p-2 rounded-md  text-white border border-black gap-2 items-center justify-center flex disabled:opacity-50 " 
     >
       {props.children}
     </button>
